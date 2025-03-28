fc7979c1d1f7e7b397dcb12e9030452311d9d23e
Swallow8801 <150134490+swallow8801@users.noreply.github.com>
2025-03-28 14:40:01 +0900

    [Silver III] Title: 알고리즘 수업 - 병합 정렬 1, Time: 3052 ms, Memory: 141720 KB -BaekjoonHub

---
+++ 백준/Silver/24060. 알고리즘 수업 － 병합 정렬 1/알고리즘 수업 － 병합 정렬 1.py
@@ -0,0 +1,54 @@
+import sys
+
+def merge_sort(s, e):
+    if (s < e):
+        m = (s + e) // 2
+        merge_sort(s, m)
+        merge_sort(m + 1, e)
+        merge(s, m, e)
+
+def merge(s, m, e):
+    i = s
+    j = m + 1 
+    t = s
+    while (i <= m and j <= e):
+        if (A[i] <= A[j]):
+            result.append(A[i])
+            tmp[t] = A[i]
+            t += 1
+            i += 1
+        else:
+            result.append(A[j])
+            tmp[t] = A[j]
+            t += 1
+            j += 1
+
+    while (i <= m):
+        result.append(A[i])
+        tmp[t] = A[i]
+        t += 1
+        i += 1
+    while (j <= e):
+        result.append(A[j])
+        tmp[t] = A[j]
+        t += 1
+        j += 1
+
+    i = s
+    t = s
+    while (i <= e):
+        A[i] = tmp[t]
+        i += 1
+        t += 1
+        
+N, K = map(int, sys.stdin.readline().split())
+A = list(map(int, sys.stdin.readline().split()))
+
+tmp = list(0 for _ in range(N))
+result = list()
+merge_sort(0, N - 1)
+
+if (K <= len(result)):
+    print(result[K - 1])
+else:
+    print(-1)
